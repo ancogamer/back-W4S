2dddfc4de4558e193141e931bed1c636229214b2
João Vitor Astori Saletti <39197618+ancogamer@users.noreply.github.com>
2020-03-06 20:11:42 -0300

    Mergin my files
    Pull origin

--- GO/controllers/handler/login.go
+++ GO/controllers/handler/login.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	
 )
 
 func LoginCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
 		c.JSON(http.StatusOK, map[string]string{
 			"Nicolas": "Bemvindo",
 		})
