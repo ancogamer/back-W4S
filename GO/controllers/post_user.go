853520eba61d15ee822305d6e7340d71016e8593
João Vitor Astori Saletti <39197618+ancogamer@users.noreply.github.com>
2020-03-13 08:58:35 -0300

    API

--- GO/controllers/post_user.go
+++ GO/controllers/post_user.go
@@ -3,7 +3,6 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"net/http"
@@ -11,12 +10,11 @@ import (
 )
 
 type CreateUserInput struct{
-	ID       uint32 `json:"id" binding:required`
 	Nickname string  `json:"nickname" binding:required`
 	Email 	 string  `json:"email" binding:required `
 	Password string  `json:"password" binding:required`
 	Name     string  `json:"name" binding:required`
-	Lastname string  `json:"string" binding:required`
+	Lastname string  `json:"lastname" binding:required`
 }
 /*type CreateProfile struct{
 	Avatar         string  `json:"avatar" ` //longtext no BD (mysql-MariaDB)
@@ -27,23 +25,23 @@ func CreateUser( c *gin.Context){
 	db:= c.MustGet("db").(*gorm.DB)
 	//Validating input
 	var input CreateUserInput
+
 	if err:= c.ShouldBindJSON(&input);err !=nil{
-		fmt.Println(err)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error":err.Error(),
 		})
 		return
 	}
-
 	//Creating user
 	user:=models.User{
-		ID: 0,
 		Nickname: input.Nickname,
 		Email:input.Email,
 		Password :input.Password,
 		Name: input.Name,
 		Lastname: input.Lastname,
 	}
+
 	db.Create(&user)
+
 	c.JSON(http.StatusOK,gin.H{"data":user})
 }
--- GO/models/user.go
+++ GO/models/user.go
@@ -4,8 +4,9 @@ package models
 type User struct {
 	ID       uint32 `json:"id" gorm:"type:bigint;primary_key; AUTO_INCREMENT"`
 	Nickname string `json:"nickname "`
-	Email    string `json:"email" `
+	Email    string `json:"email" gorm:"type:varchar(100);unique_index" `
 	Password string `json:"password"`
 	Name     string `json:"name"`
 	Lastname string `json:"string"`
+	Deleted  bool    `json:"deleted" gorm:"type:BOOLEAN"`
 }
