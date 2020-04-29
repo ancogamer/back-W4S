7c5dfc947368b3b8fb314614b7126fc27d49429b
João Vitor Astori Saletti <39197618+ancogamer@users.noreply.github.com>
2020-04-29 14:40:45 -0300

    Small Fix. Fix: CreateUser token, + gorm.Model
    ==========================================================
    
    Signed-off-by: ancogamer <39197618+ancogamer@users.noreply.github.com>

--- GO/models/tokens.go
+++ GO/models/tokens.go
@@ -1,8 +1,12 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type LogoffListTokens struct {
+	gorm.Model
 	Token string `json:"token"`
 }
 type AccountCreatedToken struct {
+	gorm.Model
 	Token string `json:"token"`
 }
