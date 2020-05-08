02c175da431d65408150dd1952f88aea76a74c83
João Vitor Astori Saletti <39197618+ancogamer@users.noreply.github.com>
2020-05-08 11:37:58 -0300

    Small Change: DB-Setup-> Using ENV variables now
    ==========================================================
    
    Signed-off-by: João Vitor Astori Saletti <39197618+ancogamer@users.noreply.github.com>

--- GO/DB/setup.go
+++ GO/DB/setup.go
@@ -4,12 +4,13 @@ package DB
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 	"w4s/models"
 )
 
 func SetupModels() *gorm.DB {
 	db, err := gorm.Open("mysql",
-		"Saletti:Saletti123@/w4s?charset=utf8&parseTime=True&loc=Local")
+		""+os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@/w4s?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic("Failed to connect to database!")
